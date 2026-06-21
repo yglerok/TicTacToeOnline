349a16ae2d5ab75ace13ebeeff059ebdb5196921
yglerok <elizaveta.gontcharova@yandex.ru>
2026-06-21 13:33:24 +0300

    feat: add index.html

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "GameManager.h"
+#include "httplib/httplib.h"
 
 #include <iostream>
 
@@ -44,16 +45,44 @@ int main()
     // Game game(0, 1);
     // game.play();
 
-    gameManager = GameManager::getInstance();
+    httplib::Server srv;
 
-    while (!end) {
-        std::cout << "==========" << std::endl;
-        std::cout << "1 - add new game\n2 - delete game\n3 - show list\n4 - exit" << std::endl;
-        std::cout << "==========" << std::endl;
-        int input;
-        std::cin >> input;
-        processInput(input);
-    }
+    // srv.Post("/", [](const httplib::Request& req, httplib::Response& res){
+    //     std::string body = req.body;
+    //     std::cout << "Recieved: " << body << std::endl;
+
+    //     res.set_content("Recieved: " + body, "text/plain");
+    // });
+
+    srv.set_mount_point("/", "./public");
+    // srv.Get("/", [](const httplib::Request &, httplib::Response &res) {
+    //     res.set_content("", "text/html");
+    // });
+
+    srv.Post("/button-click", [](const httplib::Request& req, httplib::Response& res) {
+        std::string body = req.body;
+        std::cout << "Button clicked! Message: " << body << std::endl;
+
+        res.set_content("Button click processed! Recieved: " + body, "text/plain");
+    });
+
+    srv.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello, world!", "text/plain");
+    });
+
+    std::cout << "server is running on http://127.0.0.1:8080" << std::endl;
+    srv.listen("0.0.0.0", 8080);
+
+    // gameManager = GameManager::getInstance();
+
+    // while (!end) {
+    //     std::cout << "==========" << std::endl;
+    //     std::cout << "1 - add new game\n2 - delete game\n3 - show list\n4 - exit" << std::endl;
+    //     std::cout << "==========" << std::endl;
+    //     int input;
+    //     std::cin >> input;
+    //     processInput(input);
+    // }
 
     return 0;
 }
