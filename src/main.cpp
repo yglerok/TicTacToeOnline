c26d007680f6576424c3e81ddb303a59a428de1b
yglerok <elizaveta.gontcharova@yandex.ru>
2026-08-23 22:11:20 +0300

    fix: copied /public in /build/public

--- src/main.cpp
+++ src/main.cpp
@@ -205,7 +205,7 @@ int main()
         GameManager::getInstance()->disconnectPlayer(gameId, playerId); 
     });
 
-    std::cout << "server is running on http://127.0.0.1:8080" << std::endl;
+    std::cout << "server is running on http://0.0.0.0:8080" << std::endl;
     srv.listen("0.0.0.0", 8080);
 
     return 0;
